2f372cf5e346210a32eccc7a9b3ecee51107cd65
Zoraiz Bin Zahid <zoraizbinzahid@gmail.com>
2025-10-30 00:27:21 +0500

    users urls created

---
+++ backend/users/urls.py
@@ -0,0 +1,15 @@
+from django.urls import path
+from django.contrib.auth import views as auth_views
+from . import views
+
+
+app_name = 'users'
+
+urlpatterns = [
+    path('register/', views.register_view, name='register'),
+    path('login/', auth_views.LoginView.as_view(template_name="users/login.html"), name='login'),
+    path('logout/', auth_views.LogoutView.as_view(), name='logout'),
+    path('profile/edit/', views.edit_profile_view, name='edit_profile'),
+    path('profile/<str:username>/', views.profile_view, name='profile'),
+]
+
